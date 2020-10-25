048e14cab6c14fad36856ac124b859d901a7c38d
JJ Lee <vacawind@gmail.com>
2020-10-25 16:45:42 +0800

    update file

--- chat.py
+++ chat.py
@@ -1,30 +1,27 @@
 
-output = []
+lines = []
 with open('input.txt', 'r', encoding = "utf-8") as f:
 	for line in f:
-		output.append(line.strip().strip('\ufeff'))
+		lines.append(line.strip().strip('\ufeff'))
 
 flag = 0
 chat = []
-for line in output:
-
-	# 	continue
+for line in lines:
 	if line == 'Allen':
 		flag = 1
 		continue
 	elif line == 'Tom':
-		flag =2 
+		flag = 2 
 		continue
-
 	if flag == 1:
-		chat.append(['Allen：', line])
+		chat.append( 'Allen：' + line )
 	if flag == 2:
-		chat.append(['Tom：', line])
+		chat.append( 'Tom：' + line )
 print(chat)
 
 with open('output.txt', 'w', encoding = 'utf-8') as f:
 	for line in chat:
-		f.write(line[0] + line[1] + '\n')
+		f.write(line + '\n')
 
 
 
