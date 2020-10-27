25b8b8ab2f31421a82b9f6f24c9bc9dbf27e87cb
JJ Lee <vacawind@gmail.com>
2020-10-27 21:30:44 +0800

    update file

--- chat.py
+++ chat.py
@@ -2,7 +2,7 @@
 # 讀取檔案
 def read_file(filename):
 	lines = []
-	with open('input.txt', 'r', encoding = "utf-8-sig") as f:
+	with open(filename, 'r', encoding = "utf-8-sig") as f:
 		for line in f:
 			lines.append(line.strip())
 	return lines
